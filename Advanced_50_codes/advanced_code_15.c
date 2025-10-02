bac58957d5899c8d8de7e7e646ef5b7bf4fd0827
Suhanibhosle9 <bhoslesuhani9@gmail.com>
2025-10-03 00:15:47 +0530

    Add files via upload

---
+++ Advanced_50_codes/advanced_code_15.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* next;
+};
+int main() {
+    struct Node* head = (struct Node*)malloc(sizeof(struct Node));
+    head->data = 15;
+    head->next = NULL;
+    printf("Advanced Linked List Example: %d\n", head->data);
+    free(head);
+    return 0;
+}
